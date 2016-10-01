a0632c54d1a0e556d75edbe8e2a6e421fcf86b59
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-10-01 23:30:59 +0100

    Rename to GitMate

--- gitmate/wsgi.py
+++ gitmate/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for coala_online project.
+WSGI config for GitMate project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coala_online.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gitmate.settings")
 
 application = get_wsgi_application()
