410dacee678388f7724c93eec902e7c76e26d876
Stefan Zimmermann <zimmermann.code@gmail.com>
2016-12-04 00:45:23 +0100

    Register plugins in settings and database

--- gitmate_config/apps.py
+++ gitmate_config/apps.py
@@ -1,5 +1,21 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class GitmateConfigConfig(AppConfig):
     name = 'gitmate_config'
+
+    def ready(self):
+        """
+        Update database with all configured plugins.
+        """
+        from .models import Plugin
+
+        for name in settings.GITMATE_PLUGINS:
+            try:
+                plugin = Plugin.objects.get(name=name)
+            except Plugin.DoesNotExist:
+                plugin = Plugin(name=name)
+            # check importability
+            plugin.import_module()
+            plugin.save()
--- gitmate_config/models.py
+++ gitmate_config/models.py
@@ -5,7 +5,10 @@ from django.db import models
 
 class Plugin(models.Model):
     name = models.CharField(max_length=50)
-    active = models.BooleanField()
+    active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
 
     def import_module(self):
         return import_module('gitmate_' + self.name)
