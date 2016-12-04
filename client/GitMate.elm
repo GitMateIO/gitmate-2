231f60e63dc3843a297c2f40d18649875a8982a9
Stefan Zimmermann <zimmermann.code@gmail.com>
2016-12-04 02:37:57 +0100

    Add elm-bootstrapify

--- client/GitMate.elm
+++ client/GitMate.elm
@@ -1,7 +1,35 @@
 module GitMate exposing (..)
 
-import Html
+import Bootstrap.Buttons exposing (..)
+import Bootstrap.Grid exposing (..)
+import Bootstrap.Navbar exposing (..)
+import Bootstrap.Page exposing (..)
+import Bootstrap.Wells exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
 
 
 main =
-  Html.text "Hey mana!"
+  containerFluid
+    [ navbar DefaultNavbar []
+        [ containerFluid
+            [ navbarHeader []
+                [ navbarBrand []
+                    [ text "Gitmate" ]
+                ]
+            , navbarList NavbarNav NavbarDefault []
+                [ li []
+                    [ a []
+                        [ text "Plugins" ]
+                    ]
+                ]
+            ]
+        ]
+    , jumbotron []
+        [ h1 []
+            [ text "Hey Elmana!" ]
+        , btn BtnPrimary [] [] []
+            [ text "GitLab" ]
+        ]
+    ]
