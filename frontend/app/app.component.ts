3b862bd5ee1e198df4916895e2ca9f67959798d0
Fabian Neuschmidt <fabian@neuschmidt.de>
2017-02-20 02:51:32 +0100

    frontend: Keep toolbar on top

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: `./static/app/app.component.html`,
+  styleUrls: [`./static/app/app.component.css`]
 })
 export class AppComponent  { name = 'Angular'; }
