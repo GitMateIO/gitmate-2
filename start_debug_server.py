78832dffb8ab31913e48ffd137963aa6dc875878
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-05-02 10:21:04 +0200

    start_debug_server: Make ngrok optional

--- start_debug_server.py
+++ start_debug_server.py
@@ -1,6 +1,7 @@
 from os import environ
 from subprocess import call
 from subprocess import Popen
+import sys
 from time import sleep
 
 import requests
@@ -8,9 +9,11 @@ import requests
 if __name__ == '__main__':
     print('\033[36mBE SURE TO HAVE YOUR SECRETS EXPOSED!\033[0m')
 
-    print('\033[36mgetting a tunnel so you can test GitHub webhooks...\033[0m')
-    call(['killall', 'ngrok'])
-    Popen(['ngrok', 'http', '8000', '-log', 'stdout'])
+    if '--ngrok' in sys.argv:
+        print('\033[36mgetting a tunnel so you can test GitHub webhooks...'
+              '\033[0m')
+        call(['killall', 'ngrok'])
+        Popen(['ngrok', 'http', '8000', '-log', 'stdout'])
 
     print('\033[36mprecautiously attempting to migrate database...\033[0m')
     call(['python3', 'manage.py', 'migrate'])
@@ -18,12 +21,14 @@ if __name__ == '__main__':
     print('\033[36mupmating plugins...\033[0m')
     call(['python3', 'manage.py', 'upmate'])
 
-    print('\033[36mwaiting 2s for the tunnel to start\033[0m')
-    sleep(2)
-    resp = requests.get('http://127.0.0.1:4040/inspect/http')
-    domain = resp.text[resp.text.find('\\"https://')+10:
-                       resp.text.find('.ngrok.io') + 9]
-    environ['HOOK_DOMAIN'] = domain
+    if '--ngrok' in sys.argv:
+        print('\033[36mwaiting 2s for the tunnel to start\033[0m')
+        sleep(2)
+        resp = requests.get('http://127.0.0.1:4040/inspect/http')
+        domain = resp.text[resp.text.find('\\"https://')+10:
+                           resp.text.find('.ngrok.io') + 9]
+        environ['HOOK_DOMAIN'] = domain
+
     environ['DJANGO_DEBUG'] = 'True'
 
     print('\033[36mstarting the server, you are now on your own. '
