45c56bf76425af6d2e612b3a313f37f3d2a89e0e
Naveen Kumar Sangi <nkprince007@gmail.com>
2017-05-14 20:06:29 +0530

    Add tests for PR size labeller responder

---
+++ gitmate_pr_size_labeller/tests/test_pr_size_labeller.py
@@ -0,0 +1,44 @@
+from os import environ
+from unittest.mock import patch
+from unittest.mock import PropertyMock
+
+from django.test import override_settings
+from IGitt.GitHub.GitHubIssue import GitHubIssue
+from IGitt.GitHub.GitHubMergeRequest import GitHubMergeRequest
+from rest_framework import status
+
+from gitmate_config.tests.test_base import GitmateTestCase
+
+
+@override_settings(CELERY_ALWAYS_EAGER=True)
+class TestPRSizeLabeller(GitmateTestCase):
+    def setUp(self):
+        super().setUpWithPlugin('pr_size_labeller')
+        self.github_data = {
+            'repository': {'full_name': environ['GITHUB_TEST_REPO']},
+            'pull_request': {'number': 7},
+            'action': 'synchronize'
+        }
+        self.test_labels = {
+            'size/XS': (10, 10),
+            'size/S': (100, 100),
+            'size/M': (200, 200),
+            'size/L': (400, 400),
+            'size/XL': (700, 700),
+            'size/XXL': (1000, 1000),
+        }
+
+    @patch.object(GitHubMergeRequest, 'diffstat', new_callable=PropertyMock)
+    @patch.object(GitHubIssue, 'labels', new_callable=PropertyMock)
+    def test_github(self, m_labels, m_diffstat):
+        for label, diffstat in self.test_labels.items():
+            m_diffstat.return_value = diffstat
+            response = self.simulate_github_webhook_call('pull_request',
+                                                         self.github_data)
+            self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+            # Check for label getter call
+            m_labels.assert_called()
+
+            # Check for label setter call
+            m_labels.assert_called_with({label})
