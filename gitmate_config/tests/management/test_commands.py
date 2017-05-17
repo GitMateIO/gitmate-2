aa6dee825ef12e68707ba903046cd4c032fa49ee
Naveen Kumar Sangi <nkprince007@gmail.com>
2017-05-17 19:08:57 +0530

    Rewrite test_commands with GitmateTestCase

--- gitmate_config/tests/management/test_commands.py
+++ gitmate_config/tests/management/test_commands.py
@@ -7,44 +7,40 @@ from django.core.management.base import CommandError
 import pytest
 
 from gitmate_config.models import Plugin
-from gitmate_config.models import Repository
-from gitmate_config.models import User
+from gitmate_config.tests.test_base import GitmateTestCase
 
 
-@pytest.mark.django_db
-def test_upmate():
-    # before calling upmate no plugins should be registered
-    assert not Plugin.objects.all()
-    call_command('upmate')
-    # after calling the should be only gitmate_testplugin
-    plugins = Plugin.objects.all()
-    assert len(plugins) == len(settings.GITMATE_PLUGINS)
-    testplugin = plugins[0]
-    assert testplugin.name == 'testplugin'
-    testplugin_module = testplugin.import_module()
-    assert ismodule(testplugin_module)
-    assert testplugin_module.__name__ == 'gitmate_testplugin'
-    user = User.objects.create_user(
-        username='john',
-        email='john.appleseed@example.com',
-        password='top_secret',
-        first_name='John',
-        last_name='Appleseed')
-    repo = Repository(
-        user=user, full_name='test', provider='example')
-    repo.save()
-    call_command('upmate')
-    repo.plugins.add(testplugin)
-    assert repo.get_plugin_settings() == {
-        'example_bool_setting': True,
-        'example_char_setting': 'example'
-    }
+class TestCommands(GitmateTestCase):
+    def setUp(self):
+        super().setUp(upmate=False)
 
+    def test_upmate(self):
+        # before calling upmate no plugins should be registered
+        self.assertFalse(Plugin.objects.all().exists())
+        call_command('upmate')
+        # after calling all plugins should be present
+        plugins = Plugin.objects.all()
+        self.assertEquals(len(plugins), len(settings.GITMATE_PLUGINS))
 
-def test_startplugin():
-    # pre existent plugin match
-    with pytest.raises(CommandError):
-        call_command('startplugin', 'testplugin')
+        testplugin = plugins.get(name='testplugin')
+        self.assertEquals(testplugin.name, 'testplugin')
 
-    call_command('startplugin', 'star_wars')
-    rmtree('gitmate_star_wars')
+        testplugin_module = testplugin.import_module()
+        self.assertTrue(ismodule(testplugin_module))
+        self.assertEquals(testplugin_module.__name__, 'gitmate_testplugin')
+
+        call_command('upmate')
+        self.repo.plugins.add(testplugin)
+
+        self.assertEquals(self.repo.get_plugin_settings(), {
+            'example_bool_setting': True,
+            'example_char_setting': 'example',
+        })
+
+    def test_startplugin(self):
+        # pre existent plugin match
+        with pytest.raises(CommandError):
+            call_command('startplugin', 'testplugin')
+
+        call_command('startplugin', 'star_wars')
+        rmtree('gitmate_star_wars')
