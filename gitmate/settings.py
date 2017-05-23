20e033733465ab06555cb8acf9ed29e19aca9677
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-05-23 19:24:34 +0200

    Disable CSRF
    CSRF destroys everything and our design is cross anyway.

--- gitmate/settings.py
+++ gitmate/settings.py
@@ -159,11 +159,11 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'gitmate.disable_csrf.DisableCSRF',
 ]
 
 ROOT_URLCONF = 'gitmate.urls'
