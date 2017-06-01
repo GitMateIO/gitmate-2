c0157b7302b73dbca40a171931a302fff59e69fb
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-06-01 16:39:51 +0200

    start_debug_server: Use localtunnel

--- start_debug_server.py
+++ start_debug_server.py
@@ -4,9 +4,6 @@ from os import environ
 from subprocess import call
 from subprocess import Popen
 import sys
-from time import sleep
-
-import requests
 
 if __name__ == '__main__':
     print('\033[36mBE SURE TO HAVE YOUR SECRETS EXPOSED!\033[0m')
@@ -14,22 +11,15 @@ if __name__ == '__main__':
     if '--ngrok' in sys.argv:
         print('\033[36mgetting a tunnel so you can test GitHub webhooks...'
               '\033[0m')
-        call(['killall', 'ngrok'])
-        Popen(['ngrok', 'http', '8000', '-log', 'stdout'])
+        call(['killall', 'lt'])
+        Popen(['lt', '-s', 'testgitmate', '-p', '8000'])
+        environ['HOOK_DOMAIN'] = 'testgitmate.localtunnel.me'
 
     print('\033[36mprecautiously attempting to migrate database...\033[0m')
     call(['python3', 'manage.py', 'migrate'])
 
     print('\033[36mupmating plugins...\033[0m')
     call(['python3', 'manage.py', 'upmate'])
-
-    if '--ngrok' in sys.argv:
-        print('\033[36mwaiting 2s for the tunnel to start\033[0m')
-        sleep(2)
-        resp = requests.get('http://127.0.0.1:4040/inspect/http')
-        domain = resp.text[resp.text.find('\\"https://')+10:
-                           resp.text.find('.ngrok.io') + 9]
-        environ['HOOK_DOMAIN'] = domain
 
     environ['DJANGO_DEBUG'] = 'True'
 
