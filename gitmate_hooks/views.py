3b30ddb14055b5936e78a578d2c31a3010374ce1
Naveen Kumar Sangi <nkprince007@gmail.com>
2017-06-12 22:19:00 +0530

    Add webhook receiver for GitLab
    Fixes https://gitlab.com/coala/GSoC-2017/issues/84 and
    https://gitlab.com/coala/GSoC-2017/issues/85

--- gitmate_hooks/views.py
+++ gitmate_hooks/views.py
@@ -5,6 +5,9 @@ from django.views.decorators.csrf import csrf_exempt
 from IGitt.GitHub.GitHubComment import GitHubComment
 from IGitt.GitHub.GitHubIssue import GitHubIssue
 from IGitt.GitHub.GitHubMergeRequest import GitHubMergeRequest
+from IGitt.GitLab.GitLabComment import GitLabComment
+from IGitt.GitLab.GitLabIssue import GitLabIssue
+from IGitt.GitLab.GitLabMergeRequest import GitLabMergeRequest
 from IGitt.Interfaces.Actions import IssueActions
 from IGitt.Interfaces.Actions import MergeRequestActions
 from IGitt.Interfaces.Comment import CommentType
@@ -85,5 +88,84 @@ def github_webhook_receiver(request):
                     pull_request_obj,
                     comment_obj,
                     options=repo_obj.get_plugin_settings())
+
+    return Response(status=status.HTTP_200_OK)
+
+
+@csrf_exempt
+@api_view(['POST'])
+def gitlab_webhook_receiver(request):
+    """
+    Receives webhooks from GitLab and carries out the appropriate action.
+    """
+    webhook = json.loads(request.body.decode('utf-8'))
+    event = request.META['HTTP_X_GITLAB_EVENT']
+    repository = (webhook['project'] if 'project' in webhook.keys()
+                  else webhook['object_attributes']['target'])
+    repo_obj = Repository.objects.filter(
+        active=True,
+        full_name=repository['path_with_namespace'],
+        provider=Providers.GITLAB.value).first()
+    token = repo_obj.user.social_auth.get(
+        provider=Providers.GITLAB.value).extra_data['access_token']
+
+    if event == 'Issue Hook':
+        issue = webhook['object_attributes']
+        issue_obj = GitLabIssue(
+            token, repository['path_with_namespace'], issue['iid'])
+        # todo: not clarified upon trigger event keys
+        trigger_event = {
+            'open': IssueActions.OPENED,
+            'close': IssueActions.CLOSED,
+            'reopen': IssueActions.REOPENED,
+        }.get(issue['action'], IssueActions.ATTRIBUTES_CHANGED)
+
+        ResponderRegistrar.respond(
+            trigger_event, repo_obj, issue_obj,
+            options=repo_obj.get_plugin_settings())
+
+    elif event == 'Merge Request Hook':
+        pull_request = webhook['object_attributes']
+        # TODO: clear your doubts on actions on forum
+        # https://forum.gitlab.com/t/gitlab-webhooks-documentation/8389?u=nkprince007
+        if pull_request['action'] in ['synchronize', 'open']:
+            ipull_request = GitLabMergeRequest(
+                token, repository['path_with_namespace'], pull_request['iid'])
+            ResponderRegistrar.respond(
+                MergeRequestActions.SYNCHRONIZED, repo_obj, ipull_request,
+                options=repo_obj.get_plugin_settings())
+
+            if pull_request['action'] == 'open':
+                ResponderRegistrar.respond(
+                    MergeRequestActions.OPENED, repo_obj, ipull_request,
+                    options=repo_obj.get_plugin_settings())
+
+    elif event == 'Note Hook':
+        comment = webhook['object_attributes']
+        comment_type = {
+            'MergeRequest': CommentType.MERGE_REQUEST,
+            'Commit': CommentType.COMMIT,
+            'Issue': CommentType.ISSUE,
+            'Snippet': CommentType.SNIPPET
+        }.get(comment['noteable_type'])
+
+        if comment_type == CommentType.MERGE_REQUEST:
+            mr = webhook['merge_request']
+            igitt_mr = GitLabMergeRequest(
+                token,
+                repository['path_with_namespace'],
+                mr['iid'])
+            igitt_comment = GitLabComment(
+                token,
+                repository['path_with_namespace'],
+                mr['iid'],
+                comment_type,
+                comment['id'])
+            ResponderRegistrar.respond(
+                MergeRequestActions.COMMENTED,
+                repo_obj,
+                igitt_mr,
+                igitt_comment,
+                options=repo_obj.get_plugin_settings())
 
     return Response(status=status.HTTP_200_OK)
