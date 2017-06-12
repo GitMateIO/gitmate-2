18c67a07ea0122f09220b8252fd762367ab8d8e0
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-06-12 13:09:23 +0200

    start_debug_server: Use individual domain

--- start_debug_server.py
+++ start_debug_server.py
@@ -8,12 +8,13 @@ import sys
 if __name__ == '__main__':
     print('\033[36mBE SURE TO HAVE YOUR SECRETS EXPOSED!\033[0m')
 
+    user = environ.get('USER', 'global')
     if '--ngrok' in sys.argv:
         print('\033[36mgetting a tunnel so you can test GitHub webhooks...'
               '\033[0m')
         call(['killall', 'lt'])
-        Popen(['lt', '-s', 'testgitmate', '-p', '8000'])
-        environ['HOOK_DOMAIN'] = 'testgitmate.localtunnel.me'
+        Popen(['lt', '-s', user + 'gitmate', '-p', '8000'])
+        environ['HOOK_DOMAIN'] = user + 'gitmate.localtunnel.me'
 
     print('\033[36mprecautiously attempting to migrate database...\033[0m')
     call(['python3', 'manage.py', 'migrate'])
