83691532a25aa625f9b1856aa37eccd83dc41cab
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-06-13 14:03:18 +0200

    Add utils

---
+++ gitmate/utils.py
@@ -0,0 +1,22 @@
+from contextlib import contextmanager
+
+
+@contextmanager
+def lock_igitt_object(task: str, igitt_object):
+    """
+    The IGitt object should have an .url property, so right now this will only
+    work with issues and merge requests.
+    """
+    from db_mutex import DBMutexError, DBMutexTimeoutError
+    from db_mutex.db_mutex import db_mutex
+
+    try:
+        with db_mutex(task + igitt_object.url):
+            igitt_object.refresh()
+            yield
+    except DBMutexError:
+        logging.error('Failed to acquire lock for {} for {}'.format(
+            task, igitt_object.url))
+    except DBMutexTimeoutError:
+        logging.error('Timeout while acquiring lock for {} for {}'.format(
+            task, igitt_object.url))
