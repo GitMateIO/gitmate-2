6f92d6b91e584ba1f84c02d0785a33d3c8719d09
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-06-16 16:44:14 +0200

    utils: Remove locking
    This is a hotfix

--- gitmate/utils.py
+++ gitmate/utils.py
@@ -1,24 +1,18 @@
 from contextlib import contextmanager
 
-import logging
-
 
 @contextmanager
 def lock_igitt_object(task: str, igitt_object):
     """
     The IGitt object should have an .url property, so right now this will only
     work with issues and merge requests.
-    """
-    from db_mutex import DBMutexError, DBMutexTimeoutError
-    from db_mutex.db_mutex import db_mutex
 
-    try:
-        with db_mutex(task + igitt_object.url):
-            igitt_object.refresh()
-            yield
-    except DBMutexError:  # pragma: no cover
-        logging.error('Failed to acquire lock for {} for {}'.format(
-            task, igitt_object.url))
-    except DBMutexTimeoutError:  # pragma: no cover
-        logging.error('Timeout while acquiring lock for {} for {}'.format(
-            task, igitt_object.url))
+    TEMPORARY NOT LOCKING THINGS, just yielding. We'll have to figure out a way
+    to fix :/ Check git history for original code.
+
+    django.db.utils.IntegrityError: duplicate key value violates unique
+    constraint "db_mutex_dbmutex_lock_id_key"
+    DETAIL:  Key (lock_id)=(label mrhttps://gitlab.com/gitmate-bot/anothertest/
+    merge_requests/2) already exists.
+    """
+    yield
