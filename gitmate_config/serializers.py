54c314fe06f6ed74c56e447b71d7f28bcd3c387b
Naveen Kumar Sangi <nkprince007@gmail.com>
2017-06-23 12:20:56 +0530

    gitmate_config: Add url for results GET endpoint
    - Remove additional User import in gitmate_config/serializers.py

--- gitmate_config/serializers.py
+++ gitmate_config/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from django.contrib.auth.models import User
 from gitmate_config.models import Repository
 
 
--- gitmate_config/urls.py
+++ gitmate_config/urls.py
@@ -1,5 +1,6 @@
+from django.conf.urls import url
 from rest_framework.routers import DefaultRouter
-
+from gitmate_code_analysis.views import get_analysis_results
 from .views import PluginSettingsViewSet
 from .views import RepositoryViewSet
 from .views import UserViewSet
@@ -9,4 +10,7 @@ router.register(r'repos', RepositoryViewSet, base_name='repository')
 router.register(r'plugins', PluginSettingsViewSet, base_name='settings')
 router.register(r'users', UserViewSet, base_name='users')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    url(r'^results/(?P<repo>\S+)/(?P<sha>\S+)/$',
+        get_analysis_results, name='analysis_results')
+]
