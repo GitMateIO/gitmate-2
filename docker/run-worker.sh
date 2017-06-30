a487b14f3f40a640a2e06b659620c33ac3ac0912
Muhammad Kaisar Arkhan <yuki@coala.io>
2017-06-30 21:14:38 +0700

    Migrate Docker image to Alpine
    Closes https://gitlab.com/gitmate/open-source/gitmate-2/issues/69

--- docker/run-worker.sh
+++ docker/run-worker.sh
@@ -11,18 +11,17 @@ export docker_group_id=$(stat -c "%g" /var/run/docker.sock)
 
 # Create the docker group in this container
 echo "Creating host_docker group with gid $docker_group_id ..."
-groupadd -fr -g $docker_group_id host_docker
+addgroup -S -g $docker_group_id docker
 
 # Add the daemon user to the docker group
 echo "Adding $USER to the docker group ..."
-usermod -aG $docker_group_id $USER
+addgroup $USER docker
 
 command="celery worker -A gitmate"
 command="$command --loglevel=info"
 
 export command
 
-exec su - $USER \
-        -s /bin/bash \
-        --preserve-environment \
+exec su $USER \
+        -s /bin/sh \
         -c "$command"
