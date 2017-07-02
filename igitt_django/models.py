dc2f3d22137f7b711f3500ccdb5926d23900b256
Naveen Kumar Sangi <nkprince007@gmail.com>
2017-07-02 11:07:46 +0530

    igitt_django: Add IGittBase abstract model

--- igitt_django/models.py
+++ igitt_django/models.py
@@ -1,3 +1,28 @@
+"""
+This module contains the models related to igitt_django app.
+"""
+from django.contrib.postgres import fields as psql_fields
 from django.db import models
 
-# Create your models here.
+
+class IGittBase(models.Model):
+    """
+    An abstract base model for IGitt django models.
+    """
+    extra_data = psql_fields.JSONField()
+
+    @classmethod
+    def from_igitt_instance(cls, obj):
+        """
+        A base method that creates a model from an igitt object.
+        """
+        raise NotImplementedError
+
+    def to_igitt_instance(self, raw_token: str):
+        """
+        Returns the related igitt instance.
+        """
+        raise NotImplementedError
+
+    class Meta:
+        abstract = True
