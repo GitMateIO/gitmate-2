719c2edb59ca2ca0e69b87c4be60ce109840238c
Muhammad Kaisar Arkhan <yuki@coala.io>
2017-07-02 20:42:47 +0700

    Docker: Create a beat worker

---
+++ docker/run-beat.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ ! -e "/var/run/docker.sock" ]; then
+    echo "Please mount the Docker socket to /var/run/docker.sock"
+    exit 1
+fi
+
+# Get the host's docker group id
+echo "Getting the Docker socket group owner ... "
+export docker_group_id=$(stat -c "%g" /var/run/docker.sock)
+
+# Create the docker group in this container
+echo "Creating host_docker group with gid $docker_group_id ..."
+groupadd -fr -g $docker_group_id host_docker
+
+# Add the daemon user to the docker group
+echo "Adding $USER to the docker group ..."
+usermod -aG $docker_group_id $USER
+
+command="celery beat -A gitmate"
+command="$command --loglevel=info"
+command="$command --pidfile=/tmp/celerybeat.pid"
+
+export command
+
+exec su - $USER \
+        -s /bin/bash \
+        --preserve-environment \
+        -c "$command"
