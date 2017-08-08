7a04f29cf7631253ec1d5062fbde66f3ec45d3bc
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-08-08 15:01:41 +0200

    acking properly

--- plugins/gitmate_ack/models.py
+++ plugins/gitmate_ack/models.py
@@ -1,6 +1,9 @@
+from IGitt.Interfaces.Repository import Repository as IGittRepository
+from IGitt.Interfaces.CommitStatus import Status, CommitStatus
 from django.db import models
+from django.contrib.postgres import fields as psql_fields
 
-from gitmate_config.models import SettingsBase
+from gitmate_config.models import SettingsBase, Repository
 
 
 class Settings(SettingsBase):
@@ -12,3 +15,36 @@ class Settings(SettingsBase):
         default='unack',
         max_length=100,
         help_text='Keywords for unacknowledging commits, comma separated')
+
+
+class MergeRequestModel(models.Model):
+    repo = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    number = models.IntegerField()
+    acks = psql_fields.JSONField(default={})
+
+    @property
+    def igitt_pr(self):
+        return self.repo.igitt_repo.get_mr(self.number)
+
+    @classmethod
+    def find_mrs_with_commit(cls, repository: IGittRepository, sha: str):
+        repo = Repository.from_igitt_repo(repository)
+        return cls.objects.filter(
+            repo=repo, acks__has_key=sha)
+
+    @property
+    def ack_state(self):
+        state = CommitStatus(
+            Status.SUCCESS, 'This PR is reviewed :)',
+            'review/gitmate/manual/pr', 'https://gitmate.io')
+        for commit, acked in self.acks.values():
+            if acked is False:
+                return CommitStatus(
+                    Status.FAILED, 'This PR needs work',
+                    'review/gitmate/manual/pr', 'https://gitmate.io')
+            if acked is None:
+                state = CommitStatus(
+                    Status.PENDING, 'This PR needs review',
+                    'review/gitmate/manual/pr', 'https://gitmate.io')
+
+        return state
--- plugins/gitmate_ack/responders.py
+++ plugins/gitmate_ack/responders.py
@@ -1,15 +1,48 @@
 import re
 
 from IGitt.Interfaces.Actions import MergeRequestActions
+from IGitt.Interfaces.CommitStatus import Status, CommitStatus
 from IGitt.Interfaces.MergeRequest import MergeRequest
 from IGitt.Interfaces.Comment import Comment
 
+from gitmate_config.models import Repository
 from gitmate_hooks import ResponderRegistrar
+
+from plugins.gitmate_ack.models import MergeRequestModel
 
 
 def get_keywords(string: str):
     return tuple(elem.strip().lower()
                  for elem in string.split(',') if elem.strip())
+
+
+def unack(commit):
+    commit.set_status(CommitStatus(
+        Status.FAILED, 'This commit needs work.',
+        'review/gitmate/manual', 'https://gitmate.io/'))
+
+
+def ack(commit):
+    commit.set_status(CommitStatus(
+        Status.SUCCESS, 'This commit was acknowledged.',
+        'review/gitmate/manual', 'https://gitmate.io/'))
+
+
+def pending(commit):
+    for status in commit.get_statuses():
+        if status.context == 'review/gitmate/manual':
+            return {
+                Status.FAILED: False,
+                Status.ERROR: False,
+                Status.CANCELED: False,
+                Status.SUCCESS: True,
+            }.get(status.status)
+
+    commit.set_status(CommitStatus(
+        Status.PENDING, 'This commit needs review.',
+        'review/gitmate/manual', 'https://gitmate.io'))
+
+    return None
 
 
 @ResponderRegistrar.responder(
@@ -27,12 +60,18 @@ def gitmate_ack(pr: MergeRequest,
     commits = pr.commits
     pattern = '(^{k}\s)|(\s{k}\s)|(\s{k}$)'
 
+    db_pr = MergeRequestModel.objects.filter(
+        repository=Repository.from_igitt_repo(pr.repository),
+        number=pr.number,
+    )
+
     unack_strs = get_keywords(unack_strs)
     for kw in unack_strs:
         if re.search(pattern.format(k=kw), body):
             for commit in commits:
                 if commit.sha[:6] in body:
                     commit.unack()
+                    db_pr.acks[commit.sha] = False
 
     ack_strs = get_keywords(ack_strs)
     for kw in ack_strs:
@@ -40,6 +79,9 @@ def gitmate_ack(pr: MergeRequest,
             for commit in commits:
                 if commit.sha[:6] in body:
                     commit.ack()
+                    db_pr.acks[commit.sha] = True
+
+    pr.head.set_status(db_pr.ack_state)
 
 
 @ResponderRegistrar.responder(
@@ -51,6 +93,13 @@ def add_review_pending_status(pr: MergeRequest):
     A responder to add pending status on commits on
     MergeRequest SYNCHRONIZED and OPENED event.
     """
+    db_pr = MergeRequestModel.objects.filter(
+        repository=Repository.from_igitt_repo(pr.repository),
+        number=pr.number,
+    )
+
     commits = pr.commits
     for commit in commits:
-        commit.pending()
+        db_pr.acks[commit.sha] = commit.pending()
+
+    pr.head.set_status(db_pr.ack_state)
