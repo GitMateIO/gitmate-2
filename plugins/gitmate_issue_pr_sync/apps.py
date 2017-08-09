9c796416135181611200116ff78f21ad1aeca9ac
Fabian Neuschmidt <fabian@neuschmidt.de>
2017-08-09 15:59:49 +0200

    issue_pr_sync: Update verbose name and description

--- plugins/gitmate_issue_pr_sync/apps.py
+++ plugins/gitmate_issue_pr_sync/apps.py
@@ -4,7 +4,8 @@ from gitmate.utils import PluginCategory
 
 class GitmateIssuePrSyncConfig(GitmatePluginConfig):
     name = 'plugins.gitmate_issue_pr_sync'
-    verbose_name = 'Synchronize issue labels to related PRs'
+    verbose_name = 'Synchronize labels from referenced issues'
     plugin_category = PluginCategory.PULLS
-    description = ('Synchronizes labels from issues referenced in a Pull '
-                   'Request to the Pull Request.')
+    description = ('Assigns all labels from an issue to a pull request that '
+                   'referenced the issue. Changes to the labels of the issue '
+                   'get reflected in the pull request.')
