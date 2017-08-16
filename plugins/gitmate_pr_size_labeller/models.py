58d9939d5b02a61794313715d11dacd9eb2a1021
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-08-16 14:57:10 +0200

    size_labeller: Allow setting scheme
    Fixes https://gitlab.com/gitmate/open-source/gitmate-2/issues/103

--- plugins/gitmate_pr_size_labeller/models.py
+++ plugins/gitmate_pr_size_labeller/models.py
@@ -1,6 +1,10 @@
+from django.db import models
 
 from gitmate_config.models import SettingsBase
 
 
 class Settings(SettingsBase):
-    pass
+    size_scheme = models.CharField(
+        default='size/{size}',
+        max_length=100,
+        help_text='Label scheme to use, use {size} as placeholder')
--- plugins/gitmate_pr_size_labeller/responders.py
+++ plugins/gitmate_pr_size_labeller/responders.py
@@ -10,15 +10,16 @@ from gitmate_hooks import ResponderRegistrar
     MergeRequestActions.SYNCHRONIZED,
     MergeRequestActions.OPENED,
 )
-def add_labels_based_on_size(pr: MergeRequest):
+def add_labels_based_on_size(pr: MergeRequest, size_scheme: str='size/{size}'):
     """
     Labels the pull request with size labels according to the amount of
     code touched, commits and files involved. Helps plan the review in
     advance.
     """
+    sizes = {'XXL', 'XL', 'L', 'M', 'S', 'XS'}
     with lock_igitt_object('label mr', pr):
         labels = pr.labels.difference({
-            'size/XXL', 'size/XL', 'size/L', 'size/M', 'size/S', 'size/XS'
+            size_scheme.format(size=size) for size in sizes
         })
 
         lines_added, lines_deleted = pr.diffstat
@@ -26,19 +27,19 @@ def add_labels_based_on_size(pr: MergeRequest):
         file_score = 4 * len(pr.affected_files)
 
         if commit_score + file_score + lines_added + lines_deleted <= 100:
-            pr.labels = {'size/XS'}.union(labels)
+            pr.labels = {size_scheme.format(size='XS')}.union(labels)
 
         elif commit_score + file_score + lines_added + lines_deleted <= 250:
-            pr.labels = {'size/S'}.union(labels)
+            pr.labels = {size_scheme.format(size='S')}.union(labels)
 
         elif commit_score + file_score + lines_added + lines_deleted <= 500:
-            pr.labels = {'size/M'}.union(labels)
+            pr.labels = {size_scheme.format(size='M')}.union(labels)
 
         elif commit_score + file_score + lines_added + lines_deleted <= 1000:
-            pr.labels = {'size/L'}.union(labels)
+            pr.labels = {size_scheme.format(size='L')}.union(labels)
 
         elif commit_score + file_score + lines_added + lines_deleted <= 1500:
-            pr.labels = {'size/XL'}.union(labels)
+            pr.labels = {size_scheme.format(size='XL')}.union(labels)
 
         else:
-            pr.labels = {'size/XXL'}.union(labels)
+            pr.labels = {size_scheme.format(size='XXL')}.union(labels)
