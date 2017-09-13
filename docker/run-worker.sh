332cf31ef7dd12a2fedeb52c5c2f54b56214d496
Muhammad Kaisar Arkhan <yuki@coala.io>
2017-09-13 11:28:09 +0700

    run-worker.sh: Add support for DOCKER_HOST

--- docker/run-worker.sh
+++ docker/run-worker.sh
@@ -1,24 +1,38 @@
 #!/bin/sh
 
-if [ ! -e "/var/run/docker.sock" ]; then
-    echo "Please mount the Docker socket to /var/run/docker.sock"
-    exit 1
+DOCKER_SOCKET=/var/run/docker.sock
+
+do_docker_socket() {
+    if [ ! -e $DOCKER_SOCKET ]; then
+        echo "Please mount the Docker socket to $DOCKER_SOCKET"
+        exit 1
+    fi
+
+    # Get the host's docker group id
+    echo "Getting the Docker socket group owner ... "
+    export docker_group_id=$(stat -c "%g" $DOCKER_SOCKET)
+
+    # Create the docker group in this container
+    echo "Creating host_docker group with gid $docker_group_id ..."
+    addgroup -S -g $docker_group_id host_docker
+
+    # Add the daemon user to the docker group
+    # The host_docker group might not be created if there's already a group with
+    # the id. In that case we just grep for the existing name, this should always
+    # work.
+    group=$(cat /etc/group | grep $docker_group_id | sed 's/:.*//')
+    echo "Adding $USER to the $group group ..."
+    addgroup $USER $group
+}
+
+echo "Checking if DOCKER_HOST is defined ..."
+
+if [ "x${DOCKER_HOST}" = "x" ]; then
+    echo "DOCKER_HOST is not defined, gonna use $DOCKER_SOCKET"
+    do_docker_socket
+else
+    echo "DOCKER_HOST is defined."
 fi
-
-# Get the host's docker group id
-echo "Getting the Docker socket group owner ... "
-export docker_group_id=$(stat -c "%g" /var/run/docker.sock)
-
-# Create the docker group in this container
-echo "Creating host_docker group with gid $docker_group_id ..."
-addgroup -S -g $docker_group_id host_docker
-
-# Add the daemon user to the docker group
-# The host_docker group might not be created if there's already a group with
-# the id. In that case we just grep for the existing name, this should always
-# work.
-echo "Adding $USER to the $(cat /etc/group | grep $docker_group_id | sed 's/:.*//') group ..."
-addgroup $USER $(cat /etc/group | grep $docker_group_id | sed 's/:.*//')
 
 exec celery worker \
             -A gitmate \
