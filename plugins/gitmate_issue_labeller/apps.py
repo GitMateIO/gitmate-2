1e0f746a01a1f34a1559a1e7355eda562c45ee81
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-09-15 13:20:29 +0200

    issue_labeller: Update description

--- plugins/gitmate_issue_labeller/apps.py
+++ plugins/gitmate_issue_labeller/apps.py
@@ -6,4 +6,5 @@ class GitmateIssueLabellerConfig(GitmatePluginConfig):
     name = 'plugins.gitmate_issue_labeller'
     verbose_name = 'Assign mentioned labels'
     plugin_category = PluginCategory.ISSUE
-    description = 'Assign all labels to an issue that appear in the issue description'
+    description = 'Assign all labels to an issue that appear in the issue ' \
+                  'description or based on keywords'
