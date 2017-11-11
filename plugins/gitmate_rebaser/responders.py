e31953c63d30b26a1d356d5df9c4335940416c40
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2017-11-11 16:34:41 +0530

    rebaser: Modify rebase command syntax
    Fixes https://gitlab.com/gitmate/open-source/gitmate-2/issues/218

--- plugins/gitmate_rebaser/responders.py
+++ plugins/gitmate_rebaser/responders.py
@@ -1,5 +1,5 @@
-from os import environ
 import json
+import re
 
 from django.conf import settings
 from IGitt.Interfaces.Actions import MergeRequestActions
@@ -7,7 +7,11 @@ from IGitt.Interfaces.Comment import Comment
 from IGitt.Interfaces.MergeRequest import MergeRequest
 
 from gitmate.utils import run_in_container
+from gitmate_config.models import Repository
 from gitmate_hooks.utils import ResponderRegistrar
+
+
+REBASE_COMMAND_REGEX = r'@{}\s+rebase'
 
 
 @ResponderRegistrar.responder('rebaser', MergeRequestActions.COMMENTED)
@@ -16,11 +20,10 @@ def rebase_merge_request(pr: MergeRequest, comment: Comment):
     Rebases a merge request when a user adds a rebase comment. e.g.
     ``@gitmate-bot rebase`` within the comment body.
     """
-    from gitmate_config.models import Repository
-
     username = Repository.from_igitt_repo(pr.repository).user.username
     body = comment.body.lower()
-    if '@{}'.format(username) in body and 'rebase' in body:
+    compiled_regex = re.compile(REBASE_COMMAND_REGEX.format(username))
+    if compiled_regex.search(body):
         pr.add_comment(
             'Hey! This pull request is being rebased automatically. Please DO '
             'NOT push while rebase is in progress or your changes would be '
