503814d3e1e39af03561162c76c2fc0067fbdeda
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2017-12-13 20:26:51 +0530

    hooks: Correct the URL config for /webhooks/
        Allow URL with and without trailing slash

--- gitmate_hooks/urls.py
+++ gitmate_hooks/urls.py
@@ -4,6 +4,6 @@ from .views import github_webhook_receiver
 from .views import gitlab_webhook_receiver
 
 urlpatterns = [
-    url(r'^github$', github_webhook_receiver, name='github'),
-    url(r'^gitlab$', gitlab_webhook_receiver, name='gitlab'),
+    url(r'github', github_webhook_receiver, name='github'),
+    url(r'gitlab', gitlab_webhook_receiver, name='gitlab'),
 ]
