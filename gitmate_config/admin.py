dba783a7a830ed8bd75a650cf4533a26e12774f6
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2017-12-19 18:09:28 +0530

    Add search bar to plugin settings admin UI
    - Auto-register all SettingBase subclasses

--- gitmate_config/admin.py
+++ gitmate_config/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from gitmate_config.models import Plugin, Repository, Organization, Installation
+from gitmate_config.models import Plugin
+from gitmate_config.models import Repository
+from gitmate_config.models import Organization
+from gitmate_config.models import Installation
+from gitmate_config.utils import register_all_setting_models
 
 
+register_all_setting_models()
 admin.site.register(Organization)
 admin.site.register(Plugin)
 admin.site.register(Repository)
--- plugins/gitmate_code_analysis/admin.py
+++ plugins/gitmate_code_analysis/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Settings, AnalysisResults
+from .models import AnalysisResults
 
-admin.site.register(Settings)
+
 admin.site.register(AnalysisResults)
