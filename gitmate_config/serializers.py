981901e7f3fe9f3eb841d0bb7c12d9202cd011de
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2017-12-30 15:44:05 +0530

    serializers: Add InstallationSerializer
    - Add docstrings to all serializers

--- gitmate_config/serializers.py
+++ gitmate_config/serializers.py
@@ -1,23 +1,46 @@
+"""
+This module contains the serializers for all models in gitmate core.
+"""
 from django.contrib.auth.models import User
-from rest_framework import serializers
-from django.contrib.auth.models import User
+from rest_framework.serializers import CharField
+from rest_framework.serializers import ListField
+from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import Serializer
+from rest_framework.serializers import SlugRelatedField
+from rest_framework.serializers import StringRelatedField
+
+from gitmate_config.models import Installation
 from gitmate_config.models import Repository
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(ModelSerializer):
+    """
+    A serializer for User model.
+    """
     class Meta:
         model = User
         fields = ('email', 'username', 'first_name', 'last_name')
 
 
-class RepositorySerializer(serializers.ModelSerializer):
-    user = serializers.SlugRelatedField(
-        slug_field='username',
-        queryset=User.objects.all()
-    )
-    admins = serializers.SlugRelatedField(many=True, read_only=True,
-                                          slug_field='username')
-    org = serializers.StringRelatedField(read_only=True)
+class InstallationSerializer(ModelSerializer):
+    """
+    A serializer for Installation model.
+    """
+    admins = SlugRelatedField(many=True, read_only=True, slug_field='username')
+
+    class Meta:
+        model = Installation
+        fields = '__all__'
+
+
+class RepositorySerializer(ModelSerializer):
+    """
+    A serializer for Repository model.
+    """
+    user = SlugRelatedField(slug_field='username', queryset=User.objects.all())
+    admins = StringRelatedField(many=True, read_only=True)
+    org = StringRelatedField(read_only=True)
+    installation = InstallationSerializer(read_only=True)
 
     class Meta:
         model = Repository
@@ -25,6 +48,9 @@ class RepositorySerializer(serializers.ModelSerializer):
         read_only_fields = ('id', 'admins', 'provider', 'full_name')
 
 
-class PluginSettingsSerializer(serializers.Serializer):
-    repository = serializers.CharField()
-    plugins = serializers.ListField()
+class PluginSettingsSerializer(Serializer):
+    """
+    A serializer for plugin Settings.
+    """
+    repository = CharField()
+    plugins = ListField()
