393f164d9cc2ebc2b1ede0c026e1c5e052007419
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2018-02-08 12:41:00 +0530

    Use TextFields instead of CharFields

--- plugins/gitmate_bug_spotter/models.py
+++ plugins/gitmate_bug_spotter/models.py
@@ -4,8 +4,7 @@ from gitmate_config.models import SettingsBase
 
 
 class Settings(SettingsBase):
-    pattern = models.CharField(
-        max_length=100,
+    pattern = models.TextField(
         default='(fix(e[sd])?|close[sd]?|resolve[sd]?) #[1-9][0-9]*',
         help_text='Pattern used on commit messages to identify bug fixes')
     hotspot_label = models.CharField(
