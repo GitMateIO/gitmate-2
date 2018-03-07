5aa674072ffab3bb1f0f07db21a8b500f113cd47
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2018-03-07 15:26:34 +0530

    Disable sentry logs from backoff exceptions

--- gitmate/settings.py
+++ gitmate/settings.py
@@ -26,6 +26,9 @@ from gitmate_config.enums import TaskQueue
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Disable logging backoff exceptions from IGitt to sentry cloud and instead
+# stream them to the console.
+logging.getLogger('backoff').setLevel(logging.INFO)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
